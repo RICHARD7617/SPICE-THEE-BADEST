75f87347a8e76227f46d612ea5204fab2496487c
RICHARD7617 <omondirich617@gmail.com>
2025-09-07 22:11:26 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: 'SPICE-THEE-BADDEST',
+  author: '‎',
+  botName: "SPICE-THEE-BADDEST",
+  botOwner: 'His Excellency', // Your name
+  ownerNumber: '2540116813644', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.6",
+  updateZipUrl: "https://github.com/RICHARD7617/SPICE-THEE-BADEST/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
+
